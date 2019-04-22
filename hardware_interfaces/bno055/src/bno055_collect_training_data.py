33fa72425c8d8bc75a6a37f44866d8b9f2ebbaae
Michael-Equi-RPI <equim20@student.jhs.net>
2019-04-22 16:21:27 +0000

    running imu data collector

--- hardware_interfaces/bno055/src/bno055_collect_training_data.py
+++ hardware_interfaces/bno055/src/bno055_collect_training_data.py
@@ -11,12 +11,13 @@ df = pd.DataFrame(columns=['time series', 'label'])
 #builds the timeseries and writes it to a csv file
 #each input is a tuple of lengths 3, 3, 4 respectively
 stepCounter = 0 #count steps so that each 20 samples df can be updated
-def updateArray(angular_velocity, linear_acceleration, orientation):
+def updateTimeSeries(angular_velocity, linear_acceleration, orientation):
 	#collects a finte n-dimensional time series to store IMU data for immediate analysis by nerual network
 	#once collected the time series will be stored in a csv file with any action labels that can be usde for training
 
 	#The finite time series is 2 seconds long given that the sensor is collecting data at 100Hz (timseries is 200 slots)
 	global time_series
+	global stepCounter
 	np.roll(time_series, 0)
 	#x, y, z, X, Y, Z, x`, y`, z`, w`
 	time_series[0] = [angular_velocity[0], angular_velocity[1], angular_velocity[2],
@@ -58,26 +59,26 @@ def main():
 	try:
 		status, self_test, error = sensor.get_system_status()
 	except Exception as e:
-		print('Failed to read BNO055 system status! %s', e)
+		print('Failed to read BNO055 system status! ', e)
 
 	print('System status: %s', status)
-	print('Self test result (0x0F is normal): %s', hex(self_test))
+	print('Self test result (0x0F is normal): ', hex(self_test))
 	# Print out an error if system status is in error mode.
 	if(status == 0x01):
-		print('System error: %s', error)
+		print('System error: ', error)
 		print('See datasheet section 4.3.59 for the meaning.')
 
 	# Print BNO055 software revision and other diagnostic data.
 	try:
 		sw, bl, accel, mag, gyro = sensor.get_revision()
 	except Exception as e:
-		rospy.logerr('Failed to read BNO055 meta-inforamtion! %s', e)
+		rospy.logerr('Failed to read BNO055 meta-inforamtion!', e)
 
-	print('Software version:   %d', sw)
-	print('Bootloader version: %d', bl)
-	print('Accelerometer ID:   %s', hex(accel))
-	print('Magnetometer ID:    %s', hex(mag))
-	print('Gyroscope ID:       %s', hex(gyro))
+	print('Software version:   ', sw)
+	print('Bootloader version: ', bl)
+	print('Accelerometer ID:   ', hex(accel))
+	print('Magnetometer ID:    ', hex(mag))
+	print('Gyroscope ID:       ', hex(gyro))
 
 	print('Reading BNO055 data...')
 
@@ -97,38 +98,34 @@ def main():
 				time.sleep(0.01)
 
 		if(attempts != 4):
-			info.sysCalibration = sys
-			info.accelCalibration = accel
-			info.gyroCalibration = gyro
-			info.magnoCalibration = mag
-			info.tempC = temp_c
+			print(sys)
+			#info.accelCalibration = accel
+			#info.gyroCalibration = gyro
+			#info.magnoCalibration = mag
+			#info.tempC = temp_c
 
 
 		attempts = 0
 		while(attempts < 4):
 			try:
     				# Orientation as a quaternion:
-				orientation.x, orientation.y, orientation.z, orientation.w  = sensor.read_quaternion()
+				orientation = sensor.read_quaternion()
 
 				# Gyroscope data (in degrees per second converted to radians per second):
-				gry_x, gry_y, gry_z = sensor.read_gyroscope()
-				angular_vel.x = math.radians(gry_x)
-				angular_vel.y = math.radians(gry_y)
-				angular_vel.z = math.radians(gry_z)
+				angular_vel = sensor.read_gyroscope()
 
 				# Linear acceleration data (i.e. acceleration from movement, not gravity--
     				# returned in meters per second squared):
-				linear_accel.x, linear_accel.y, linear_accel.z = sensor.read_linear_acceleration()
+				linear_accel = sensor.read_linear_acceleration()
 				break
 			except Exception as e:
-				rospy.logdebug('Failed to read BNO055 data! %s', e)
+				print('Failed to read BNO055 data!', e)
 				attempts += 1
-				rospy.sleep(0.01)
+				time.sleep(0.01)
 
 		if(attempts != 4):
-			msg.orientation = orientation
-			msg.angular_velocity = angular_vel
-			msg.linear_acceleration = linear_accel
+			print(angular_vel)
+			updateTimeSeries(angular_vel, linear_accel, orientation)
 
 
 		time.sleep(0.05)
